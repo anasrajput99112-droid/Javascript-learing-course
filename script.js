810b21c5d113fe168eee34f6e1c0911d0986e95b
Anas <anasrajput9911@gmail.com>
2026-01-13 17:32:56 +0500

    add-newcommit

--- script.js
+++ script.js
@@ -4,3 +4,10 @@
 //var a; without value delcare karna
 //var b = 20; with value delcare and initialize 
 
+function  random(){
+    var a = 10; 
+    var b = 20;
+    console.log(a+b);
+    
+}
+random();
