c5a92ad3aefd5640a81b7f96b8952db5619f8ba1
Anas <anasrajput99112@gmail.com>
2026-01-14 18:07:40 +0500

    new

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
-//var let const
+//var, let, const
 //declarations and inititalization
 
 //var a; without value delcare karna
-//var b = 20; with value delcare and initialize 
+//var b = 20; with value delcare and initialize
 
-function  random(){
-    var a = 10; 
-    var b = 20;
-    console.log(a+b);
-    
-}
-random();
+//var window main add hota ha
+//function scoped hota ha
+
+// let can be change
+// const can not be change
+
+// scope (global, block, function)
+// global scope means you can use varraible in whole code
+// function scope
+// let use in parent baseses
+//------------------------------------------------------//
+
+// Reassigment, redeclaration
+
+// temporal dead zone untna area jitna mein js ko pata ha ka ke variable exist karta hai par wo apko value nahi de sakta
+/*console.log(a);
+let a = 12;*/
+
+// Hoisting impact per type 
+
+/* Hoisting --> ek variable ko jab javascript ma bannata hai to wo variable 
+   do hissa main toot jaata hai and declare part upar chala jaata hai and 
+ uska declare part upar chala jaata hai and uska initialization part neacha raha jata ha*/
+
+
+// var a = undefined; declare part upar chala jata ha
+// a = 12; and initialization necha raha jata ha
